865ae128e789fc6753ed3d083115b5a40f422004
Shaheer Khan <119394707+shaheerkhan8476@users.noreply.github.com>
2024-12-20 00:36:59 -0500

    Simple Endpoint Malware
    Cases for POST and GET for endpoint

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
+type Task struct {
+	Name        string
+	Description string
+}
+
+var Tasks []Task = []Task{}
+
 func exampleEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("exampleEndpoint Hit")
+	switch r.Method {
+	case "POST":
+		var newTask Task
+		bodyBytes, _ := io.ReadAll(r.Body)
+
+		err := json.Unmarshal(bodyBytes, &newTask)
+		if err != nil {
+			fmt.Println(err)
+		}
+		Tasks = append(Tasks, newTask)
+	case "GET":
+		w.Write([]byte(fmt.Sprintf("%v", Tasks)))
+	}
+
 	w.WriteHeader(http.StatusOK)
 }
 func main() {
-	fmt.Println("Hello")
 	m := http.NewServeMux()
 	m.HandleFunc("/exampleEndpoint", exampleEndpoint)
 	err := http.ListenAndServe(":8080", m)
