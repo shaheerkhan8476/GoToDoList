8a429a9654ca716d9664ce276ca84fcf42891dab
Shaheer Khan <119394707+shaheerkhan8476@users.noreply.github.com>
2024-12-20 22:35:24 -0500

    endpoint fix

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	m.HandleFunc("/addTask", task.AddTask).Methods("POST")
 	m.HandleFunc("/editTask/{id}", task.EditTask).Methods("PATCH")
 	m.HandleFunc("/getTasks", task.GetTasks).Methods("GET")
-	m.HandleFunc("/getTasks/{id}", task.GetTask).Methods("GET")
+	m.HandleFunc("/getTask/{id}", task.GetTask).Methods("GET")
 	m.HandleFunc("/deleteTask/{id}", task.DeleteTask).Methods("DELETE")
 	err := http.ListenAndServe(":8080", m)
 	if err != nil {
